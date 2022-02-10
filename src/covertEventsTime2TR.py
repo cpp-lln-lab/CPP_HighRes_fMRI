1af552bdcd0e362e15baa3401e4ac8b3b885fd47
marcobarilari <marcobarilr@gmail.com>
2022-02-10 13:30:00 +0100

    refactor names to real data

--- src/covertEventsTime2TR.py
+++ src/covertEventsTime2TR.py
@@ -1,69 +1,79 @@
 import numpy as np
 import pandas as pd
+import glob
+import os
+
 
 boldTR = 2.25
 vasoTR = 1.6
 
-eventsTsv = pd.read_table('/Users/barilari/data/temp_to_delete/ses-007_bold-classic-noInterp/derivatives/cpp_spm-preproc/sub-pilot001/ses-007/func/sub-pilot001_ses-007_task-visualLocalizerFingerTapping_run-001_events.tsv', sep='\t', header=0)
+path = "/Users/barilari/data/temp_to_delete/ses-007_bold-classic-noInterp/derivatives/cpp_spm-preproc/sub-pilot001/ses-007/func/"
 
-end = np.add(eventsTsv["onset"], eventsTsv["duration"])
+files = glob.glob(path + "/*events.tsv")
 
-eventsTsv["end"] = end
+print("\n      Will covert this files:")
 
+for file in files:
+    print(file)
 
+for file in files:
 
+    eventsTsv = pd.read_table(file, sep="\t", header=0)
 
+    offset = np.add(eventsTsv["onset"], eventsTsv["duration"])
+
+    runDuration = offset[len(offset) - 1]
+
+    nbVasoBoldCouple = round(runDuration / (boldTR + vasoTR))
+
+    volumeList = ["vaso", "bold"] * nbVasoBoldCouple
+
+    volumeIdx = list(range(1, len(volumeList) + 1))
+
+    volumeEvents = [0]
+
+    for iVol in volumeIdx:
+
+        print(iVol)
+
+        if (iVol % 2) == 1:
+
+            volumeEvents.append(round(volumeEvents[iVol - 1] + vasoTR, 2))
+
+        else:
+
+            volumeEvents.append(round(volumeEvents[iVol - 1] + boldTR, 2))
+
+        print(volumeEvents)
+
+        volumeType = []
+
+        iTime = 0
+
+        for iVol in range(len(volumeIdx)):
+
+            if volumeEvents[iVol] < offset[iTime]:
+
+                volumeType.append(eventsTsv["trial_type"][iTime])
+
+            else:
+
+                iTime = iTime + 1
+
+                volumeType.append(eventsTsv["trial_type"][iTime])
+
+    print(
+        "{} within {} then {}".format(
+            volumeEvents[iVol], offset[iTime], eventsTsv["trial_type"][iTime]
+        )
+    )
+    print("volume: {}; event {}".format(iVol, iTime))
 # -------------------------------------------------------------------------------------
 
 # mockEventsStarts = [5.064472,	35.536482,	65.939492,	96.094513,	126.47973,	156.608455,	187.015942]
 
-# mockDuration = [28.336928, 28.277807, 28.032229, 28.262789,	28.009508, 28.284508, 28.017616] 
+# mockDuration = [28.336928, 28.277807, 28.032229, 28.262789,	28.009508, 28.284508, 28.017616]
 
 # mockTrialType = [ 'static', 'motion' ] * len(mockEventsStarts)
 
-mockEventsEnd = np.add(mockEventsStarts, mockDuration)
-
-runDuration = mockEventsEnd[len(mockEventsEnd)-1]
-
-nbVasoBoldCouple = round(runDuration / (boldTR + vasoTR))
-
-volumeList = [ 'vaso', 'bold' ] * nbVasoBoldCouple
-
-volumeIdx = list(range(1, len(volumeList)+1))
-
-volumeEvents = [ 0 ]
-
-for iVol in volumeIdx:
-
-  print(iVol)
-    
-  if (iVol % 2) == 1:
-
-      volumeEvents.append(round(volumeEvents[iVol - 1] + vasoTR, 2)) 
-
-  else:
-
-      volumeEvents.append(round(volumeEvents[iVol - 1] + boldTR, 2)) 
-
-print(volumeEvents)
-
-volumeType = []
-
-iTime = 0
-
-for iVol in range(len(volumeIdx)):
-
-    if volumeEvents[iVol] < mockEventsEnd[iTime]:
-
-        volumeType.append(mockTrialType[iTime])
-
-    else:
-  
-        iTime = iTime +1
-
-        volumeType.append(mockTrialType[iTime])
-
-    print('{} within {} then {}'.format(volumeEvents[iVol], mockEventsEnd[iTime], mockTrialType[iTime]))
-    print('volume: {}; event {}'.format(iVol, iTime))
-
- 
+# mockEventsEnd = np.add(mockEventsStarts, mockDuration)
